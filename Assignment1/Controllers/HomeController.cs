40b1f6181209b4dfb11e0f0214a2ab0faaf14ba4
jayant <jayantgaikwad2312@gmail.com>
2022-01-17 17:46:51 +0530

    first day work

---
+++ Assignment1/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(string arg)
+        {
+            if(arg=="sample")
+            {
+                return File("~/SYNOPSIS_JS.pdf","application/pdf");
+
+            }
+            else if(arg=="gotoabout")
+            {
+                return RedirectToAction("about");
+            }
+            else if(arg=="login")
+            {
+                return View("login");
+            }
+            else
+            {
+                return Content("you entered " + arg);
+            }
+        }
+         public ActionResult about()
+        {
+            return Content("about content here");
+        }
+    }
+}
