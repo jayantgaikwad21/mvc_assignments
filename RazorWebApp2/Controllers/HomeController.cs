759eb4fca2bc5067062df8d1bc8a3daeee5e0db0
jayant <jayantgaikwad2312@gmail.com>
2022-01-18 19:36:42 +0530

    daily work

---
+++ RazorWebApp2/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RazorWebApp2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Products()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult StudentDetails()
+        {
+            ViewBag.StudentId = 101;
+            ViewBag.StudentName = "jayant";
+            ViewBag.Marks = 80;
+            ViewBag.NoOfSemister = 6;
+            ViewBag.Subject = new List<string>() { "chem", "phy", "math" };
+            return View();
+        }
+
+    }
+}
---
+++ RazorWebApp2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RazorWebApp2.MvcApplication" Language="C#" %>
