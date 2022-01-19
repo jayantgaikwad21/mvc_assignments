3a8a8ffd798e7b819cbc11f376a7eeb4e9207176
jayant <jayantgaikwad2312@gmail.com>
2022-01-19 19:42:08 +0530

    third day work

---
+++ LayoutDemo/LayoutDemo/Controllers/homeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LayoutDemo.Controllers
+{
+    public class homeController : Controller
+    {
+        // GET: home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Profile()
+        {
+            return View();
+        }
+
+    }
+}
