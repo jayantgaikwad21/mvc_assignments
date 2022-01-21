8a326b8e5db2f38a5396463086ed3f5c5daf5198
jayant <jayantgaikwad2312@gmail.com>
2022-01-21 20:41:05 +0530

    day five work

---
+++ ModelDemo/Controllers/ProductsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ModelDemo.Models;
+namespace ModelDemo.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            List<Product> product = new List<Product>()
+            {
+                new Product{ProductId=1,ProductName="Laptop",ProductRate=60000},
+                 new Product{ProductId=2,ProductName="BMW",ProductRate=90000},
+                  new Product{ProductId=3,ProductName="Bike",ProductRate=70000}
+
+            };
+            ViewBag.product = product;
+            return View();
+
+        }
+          public ActionResult Details(int id)
+        {
+            List<Product> product = new List<Product>()
+            {
+                new Product{ProductId=1,ProductName="Laptop",ProductRate=60000},
+                 new Product{ProductId=2,ProductName="BMW",ProductRate=90000},
+                  new Product{ProductId=3,ProductName="Bike",ProductRate=70000}
+
+            };
+            Product matchingProd=null;
+            foreach(var item in product)
+            {
+                if (item.ProductId == id)
+                {
+                    matchingProd = item;
+                }
+             }
+            ViewBag.MatchingProduct= matchingProd;
+            return View();
+        }
+
+    }
+}
---
+++ ModelDemo/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ModelDemo.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public String ProductName { get; set; }
+        public double ProductRate { get; set; }
+    }
+}
